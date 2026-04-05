2ba9b687fc2818f83dd3179a284511887eb16117
pramodphd <pramodph2103@gmail.com>
2026-04-05 18:51:47 +0530

    new message

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Pramod! Your Azure App is running"
+    return "Aparnaa Love Pramod very deeply , she will kiss him very deeply till his life"
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
